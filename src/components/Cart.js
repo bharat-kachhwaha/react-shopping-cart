d700c1c1aa886a7ea6efbbf8890e4da22504a256
bharat-kachhwaha <bharat.kachhwaha@yahoo.com>
2021-03-14 02:23:42 +0530

    checkout form

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,17 +2,42 @@ import React, { Component } from 'react'
 import formatCurrency from '../util';
 
 export default class Cart extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            name:"",
+            email:"",
+            address:"",
+            showCheckout:false
+        }
+    }
+    createOrder = (e) => {
+        e.preventDefault();
+        const order = {
+            name: this.state.name,
+            email:this.state.email,
+            address:this.state.address,
+            cartItems:this.props.cartItems
+        };
+        this.props.createOrder(order);
+    }
+    handleInput = (e) => {
+        this.setState({
+            [e.target.name] : [e.target.value]
+        });
+    }
     render() {
         const {cartItems} = this.props;
         return (
             <div>
-                {cartItems.length === 0 ? 
-                    <div className="cart cart-header">Cart is Empty</div> : 
-                    <div className="cart cart-header">You have {cartItems.length} in the cart</div>
-                }
+                    {cartItems.length === 0 ? 
+                        <div className="cart cart-header">Cart is Empty</div> : 
+                        <div className="cart cart-header">You have {cartItems.length} in the cart</div>
+                    }
                 
-                <div>
-                    <div className="cart">
+                    <div>
+                        <div className="cart">
                         <ul className="cart-items">
                             {cartItems.map(item => (
                                 <li key={item._id}>
@@ -33,16 +58,41 @@ export default class Cart extends Component {
                         </ul>
                     </div>
                     {cartItems.length !== 0 && (
+                        <div>
                     <div className="cart">
                         <div className="total">
                             <div>
                                 Total:{" "}
                                 {formatCurrency(cartItems.reduce((a,c) => a + (c.price*c.count), 0))}
                             </div>
-                            <button className="button primary">
+                            <button className="button primary" onClick={() => {this.setState({showCheckout:true})}}>
                                 Proceed
                             </button>
                         </div>
+                    </div>
+                    {this.state.showCheckout && (
+                        <div className="cart">
+                            <form onSubmit={this.createOrder}>
+                                <ul className="form-container">
+                                    <li>
+                                        <label>Email</label>
+                                        <input name="email" type="email" required onChange={this.handleInput}></input>
+                                    </li>
+                                    <li>
+                                        <label>Name</label>
+                                        <input name="name" type="text" required onChange={this.handleInput}></input>
+                                    </li>
+                                    <li>
+                                        <label>Address</label>
+                                        <input name="address" type="text" required onChange={this.handleInput}></input>
+                                    </li>
+                                    <li>
+                                        <button type="submit" className="button primary">Checkout</button>
+                                    </li>
+                                </ul>
+                            </form>
+                        </div>
+                    )}
                     </div>
                     )}
                     
